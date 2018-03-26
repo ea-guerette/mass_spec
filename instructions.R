cdf0c7b068ea1e97b3e6491c600a256071401a84
Jesse Greenslade <jwg366@uowmail.edu.au>
2018-03-27 09:58:03 +1100

    added nci run script, and added prereq package

--- instructions.R
+++ instructions.R
@@ -6,5 +6,5 @@ require(MSeasy)
 load("initial_DATA.RData")
 #you can now run Step 2
 
-MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = 1500, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
+MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = 2000, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
 
--- package_install.R
+++ package_install.R
@@ -1,12 +1,8 @@
 #this code is to hopefully install the package MSeasy on NCI
 
-#check whether the package is there by trying to load it:
-require(MSeasy)
-
-
 #if error, then install dependencies
 source("https://bioconductor.org/biocLite.R")
-biocLite(c("mzR", "fpc"))
+biocLite(c("mzR", "fpc", "xcms"))
 
 #then install package itself
 install.packages("MSeasy", repos = "https://cran.csiro.au/")
