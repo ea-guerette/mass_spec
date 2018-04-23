d7df4217dac828e90094268750090e1f342173e3
Elise-Andree Guerette <eg3324@raijin2.(none)>
2018-04-23 15:41:10 +1000

    Cataract results with 6000 clusters

--- instructions.R
+++ instructions.R
@@ -6,5 +6,5 @@ require(MSeasy)
 load("data/cataract_201803.RData")
 #you can now run Step 2
 
-MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = 5000, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
+MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = 6000, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
 
