4a0d4dcfef95ca968dc16e57acb181093eeb8c2b
ea-guerette <eag873@uowmail.edu.au>
2018-07-24 10:43:31 +1000

    Preparing to run clustering on all samples (nov - feb) and cals from March

--- instructions.R
+++ instructions.R
@@ -3,8 +3,8 @@ require(MSeasy)
 #we will have to set up the directory - not sure how it works on NCI - the command is setwd("C:/")
 
 #to load in data matrix: 
-load("data/cataract_201803.RData")
+load("data/all.RData")
 #you can now run Step 2
 
-MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = 6000, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
+MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = length(mydata$analysis)-1, varRT = 60, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
 
