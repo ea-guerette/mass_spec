3c67764ab5a8706f51181f94ceeb22c709c48ca7
ea-guerette <eag873@uowmail.edu.au>
2018-07-26 12:58:39 +1000

    Trying >36 <40 on all 2 (no MAR files)

--- instructions.R
+++ instructions.R
@@ -3,11 +3,11 @@ require(MSeasy)
 #we will have to set up the directory - not sure how it works on NCI - the command is setwd("C:/")
 
 #to load in data matrix: 
-load("data/all.RData")
+load("data/all2.RData")
 #you can now run Step 2
 
 mydata <- subset(mydata, retention_time >26 & retention_time <40)
 
 MS.clust(mydata, quant = TRUE, clV = TRUE, ncmin = 10, ncmax = length(mydata$analysis)-1, varRT = 2, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
 
-#MS.clust(mydata, quant = TRUE, clV = F, Nbc = 89, varRT = 2, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
+#MS.clust(mydata, quant = TRUE, clV = F, Nbc = 43, varRT = 2, disMeth = "euclidian", linkMeth = "ward", clustMeth = "hierarchical")
